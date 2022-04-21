cc181d4c4a5416e8fff0eab39c1ff03be58fa88e
nboonyasitin <nboonyasitin@gmail.com>
2022-04-20 22:55:04 -0500

    established connection to database

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html>
-<body>
+    <body>
+        <head>
+            <h1>EECS647: MyCloud - Library Database</h1>
+        </head>
+
+    <?php
+        echo "PHP: Hello World!!\n";
+        echo "<br>";
+        echo "<br>";
+
+        $servername = "mysql.eecs.ku.edu";
+        $database = "n578b202";
+        $username = "n578b202";
+        $password = "uhaip9ei";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "Cannot connect to database";
+        }
+        echo "Connected to database";
+        mysqli_close($conn);
+
+    ?>
 
 
-<?php
-echo "Hello World!";
-?>
-
-
-</body>
+    </body>
 </html>
