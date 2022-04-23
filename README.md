800971605c0a923d182f2d4c724fd26c28ac8c26
nboonyasitin <nboonyasitin@gmail.com>
2022-04-23 16:12:26 -0500

    revert changes

--- README.md
+++ README.md
@@ -6,8 +6,3 @@ Setting up the Environment:
 2. Download Apache Server service: https://www.apachefriends.org/download.html
 3. Navigate to " htdocs " and clone the repository from inside " htdocs ". (Navigate from XAMPP >> xampfiles >> htdocs)
 4. Go to your web browser of choice and type: " http://localhost/EECS647-Project1/index.php ". You should now be able to see the output of index.php!
-
-PHP reference materials:
-
-- General reference materials: https://www.w3schools.com/php/
-- Website reference materials: https://www.php.net/manual/en/tutorial.php
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <head>
+            <h1>EECS647: MyCloud - Library Database</h1>
+        </head>
+
+    <?php
+        echo "PHP: Hello World!!!!!!!\n";
+        echo "<br>";
+        echo "<br>";
+
+        $servername = "mysql.eecs.ku.edu"; //TODO: Create a new mysql database just for the course project
+        $database = "n578b202";
+        $username = "n578b202";
+        $password = "uhaip9ei";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "Cannot connect to database";
+        }
+
+        // Create database
+        $sql = "CREATE DATABASE n578b202";
+        if ($conn->query($sql) === TRUE) {
+            echo "Database created successfully";
+        } 
+        else {
+            echo "Error creating database: " . $conn->error;
+        }
+
+        echo "Connected to database";
+        mysqli_close($conn);
+    ?>
+
+    </body>
+</html>
