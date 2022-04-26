e6fcfdccf84177cde19f4b50706a4f330c64b175
Ellen VandeWater <evandewater515@ku.edu>
2022-04-26 16:52:28 -0500

    real basic menu stuff

--- index.php
+++ index.php
@@ -3,15 +3,23 @@
     <body>
         <head>
             <h1>EECS647: MyCloud - Library Database</h1>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Chicle&display=swap" rel="stylesheet">
         </head>
 
     <?php
-        echo "PHP: Hello World!!!!!!!\n";
-        echo "<br>";
-        echo "<br>";
+        #echo "PHP: Hello World!!!!!!!\n";
+        echo "<h1>Welcome to Cloudy Day Public Libraries!</h1>";
+        echo "<div>";
+        echo "<h2 class='menu1'>Locations</h2>";
+        echo "<h2 class='menu1'>Member Lookup</h2>";
+        echo "<h2 class='menu1'>Book Lookup</h2>";
+        echo "<h2 class='menu1'>Book Due Date</h2>"; #Idk what we want to do for the borrow book part so ill just leave this here for rn.
+        echo "</div>";
 
         $servername = "mysql.eecs.ku.edu"; //TODO: Create a new mysql database just for the course project
-        $database = "n578b202";
+        $database = "n578b202"; 
         $username = "n578b202";
         $password = "uhaip9ei";
 
@@ -38,4 +46,18 @@
     ?>
 
     </body>
+    <style>
+        h1{
+            color: #0da2ff;
+            text-align: center;
+            font-family: 'Chicle', cursive;
+            font-size: 45px;
+        }
+        .menu1{
+            color: #1261a0;
+            text-align: center;
+            font-family: 'chicle', cursive;
+            font-size: 30px;
+        }
+    </style>
 </html>
