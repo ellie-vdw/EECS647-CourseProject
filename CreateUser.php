1b3866d3477af341736dde5d12e2bacd12c940ad
Ellen VandeWater <evandewater515@ku.edu>
2022-04-27 16:50:11 -0500

    create user and other bs

--- CreateUser.php
+++ CreateUser.php
@@ -6,15 +6,24 @@
 		printf("Connect failed: %s\n", $mysqli->connect_error);
 		exit();
 	}
-	$query = "SELECT LibraryCardNum FROM Members";
-	$result = $mysqli->query($query);
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo "Library Card Number: " . $row["LibraryCardNum"]. "<br>";
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$passQ = "SELECT Password FROM Members WHERE Password = '$password' AND LibraryCardNum = '$username'";
+	$presults = $mysqli->query($passQ);
+	if($presults->num_rows > 0){
+		$query = "SELECT LibraryCardNum,  MemberName, PhoneNum, Address, MemberEmail, CreditCardInfo, BuildingNum FROM Members WHERE LibraryCardNum = '$username'";
+		$result = $mysqli->query($query);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "Library Card Number: " . $row["LibraryCardNum"]. " Name: " .$row["MemberName"]. " Phone Number: " .$row["PhoneNum"]. "Address: " .$row["Address"]. "Email: " .$row["MemberEmail"]. " Library Number: " .$row["BuildingNum"]. "<br>";
+			}
+		} 
+		else {
+			echo "0 results";
 		}
-	} 
-	else {
-		echo "0 results";
+	}
+	else{
+		echo "No results found, please try again";
 	}
 	$mysqli->close();
 
