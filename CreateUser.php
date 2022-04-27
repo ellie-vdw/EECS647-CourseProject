ea2aa499e9eebfc14f759b3964ba1f245b58e609
Ellen VandeWater <evandewater515@ku.edu>
2022-04-27 15:04:41 -0500

    database changes and stylesheet

--- CreateUser.php
+++ CreateUser.php
@@ -1,38 +1,21 @@
 <?php
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "ellenvandewate", "raoThah7","ellenvandewate");
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+	$query = "SELECT LibraryCardNum FROM Members";
+	$result = $mysqli->query($query);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo "Library Card Number: " . $row["LibraryCardNum"]. "<br>";
+		}
+	} 
+	else {
+		echo "0 results";
+	}
+	$mysqli->close();
 
-    // $servername = "mysql.eecs.ku.edu"; 
-    // $database = "n578b202";
-    // $username = "n578b202";
-    // $password = "uhaip9ei";
-
-    // Create connection
-    // $conn = mysqli_connect($servername, $username, $password, $database);
-    $mysqli = new mysqli("mysql.eecs.ku.edu", "n578b202", "uhaip9ei", "n578b202");
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        printf("Cannot connect to database");
-    }
-
-    // $username = $_POST["username"];
-
-    // $query = "SELECT user_id FROM Users where user_id='".$username."'";
-
-    // if($_POST["username"] != '' && $num_rows < 1){
-    //     $query = "INSERT INTO Users (user_id) VALUES (\"$username\");";
-
-    //     if ($mysqli->query($query) == TRUE){
-    //         printf("Username saved.");     
-    //     } 
-    //     else{
-    //         printf("Username already in use.");
-    //     }
-    // } 
-    // else{
-    //     printf("Usernames cannot be empty.");
-    // }
-    printf("Connected to database");
-    /* close connection */
-    $mysqli->close();
 ?>
